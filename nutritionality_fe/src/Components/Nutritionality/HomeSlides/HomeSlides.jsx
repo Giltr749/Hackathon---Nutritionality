b16f547e07e70e169e638f65939d9a4b8926ff00
Itay Sagy <itayemails@gmail.com>
2022-05-19 00:06:22 +0300

    f

--- nutritionality_fe/src/Components/Nutritionality/HomeSlides/HomeSlides.jsx
+++ nutritionality_fe/src/Components/Nutritionality/HomeSlides/HomeSlides.jsx
@@ -1,16 +1,27 @@
-import ImageCarousel from '../../General/Carousels/ImageCarousel';
+import { useEffect, useState } from "react";
 import Col from "../../General/Flexboxes/Column/Col";
-import home_page_food_image from '../../../Images/home_page_food_image.jpg';
 import './HomeSlides.css';
 
 function HomeSlides() {
+    const [currentLocation, setCurrentLocation] = useState({ latitude: '', longitude: '' });
+
+    useEffect(() => {
+        calculateCurrentLocation();
+    }, []);
+
+    const calculateCurrentLocation = () => {
+        navigator.geolocation.getCurrentPosition((position) => {
+            setCurrentLocation(prevState => ({
+                ...prevState,
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude
+            }));
+        });
+    };
+
     return (
         <Col styles='home-slides-container' >
-            <Col styles='home-slides-inner-container'>
-                <ImageCarousel
-                    images={[home_page_food_image]}
-                />
-            </Col>
+            <iframe style={{ height: "100%", width: "100%" }} src={`http://maps.google.com/maps?q=${currentLocation.longitude},${currentLocation.latitude}&z=16&output=embed`}></iframe>
         </Col >
     );
 }

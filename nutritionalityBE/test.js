0c41fb3a52c4acfa3df2c8935c4f3c2227559b4e
Giltr749 <97340782+Giltr749@users.noreply.github.com>
2022-05-18 19:00:44 +0300

    asdf

--- nutritionalityBE/test.js
+++ nutritionalityBE/test.js
@@ -8,29 +8,29 @@ const app = express();
 const navigator = new Navigator();
 
 
-    const findLoc = async () => {
-        return new Promise((resolve, reject) => {
-            navigator.geolocation.getCurrentPosition(
-                position => resolve(position),
-                error => reject(error)
-            )
-        });
-    }
+const findLoc = async () => {
+    return new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(
+            position => resolve(position),
+            error => reject(error)
+        )
+    });
+}
 
 
 
-app.get('/nearby', async (req, res) => {
-    const location = await findLoc();
-    const keyWord = 'restaurant';
-    const locationSearch = `${location.latitude}%2C${location.longitude}`;
-    console.log(locationSearch);
-    const radius = '500';
-    const type = 'restaurant';
-    const key = 'AIzaSyCDbMPheigEhLZzpQbWBSMT33_qRKpoyAA';
-    const URL = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?keyword=${keyWord}&location=${locationSearch}&radius=${radius}&type=${type}&key=${key}`;
-    const response = await axios.get(URL);
-    // console.log(response.data.results);
-});
+// app.get('/nearby', async (req, res) => {
+//     const location = await findLoc();
+//     const keyWord = 'restaurant';
+//     const locationSearch = `${location.latitude}%2C${location.longitude}`;
+//     console.log(locationSearch);
+//     const radius = '20000';
+//     const type = 'restaurant';
+//     const key = 'AIzaSyCDbMPheigEhLZzpQbWBSMT33_qRKpoyAA';
+//     const URL = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?keyword=${keyWord}&location=${locationSearch}&radius=${radius}&type=${type}&key=${key}`;
+//     const response = await axios.get(URL);
+//     console.log(response.data.results);
+// });
 
 app.get('/', async (req, res) => {
     const asdf = '[{"text": "Hello World!"}]';
@@ -42,6 +42,31 @@ app.get('/', async (req, res) => {
             console.log(err);
             res.send(":(")
         });
+});
+
+app.get('/test', (req, res) => {
+    const encodedParams = new URLSearchParams();
+    encodedParams.append("language", "en_US");
+    encodedParams.append("limit", "30");
+    encodedParams.append("location_id", "297704");
+    encodedParams.append("currency", "USD");
+
+    const options = {
+        method: 'POST',
+        url: 'https://worldwide-restaurants.p.rapidapi.com/search',
+        headers: {
+            'content-type': 'application/x-www-form-urlencoded',
+            'X-RapidAPI-Host': 'worldwide-restaurants.p.rapidapi.com',
+            'X-RapidAPI-Key': '8f217f8828msh90af3811d263397p1fdb9ajsnccdd8074f156'
+        },
+        data: encodedParams
+    };
+
+    axios.request(options).then(function (response) {
+        console.log(response.data);
+    }).catch(function (error) {
+        console.error(error);
+    });
 });
 
 app.listen(8080, () => {
